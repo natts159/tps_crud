a097d5460c6030c84e16b3892adcc27690f27a88
Nati <medaigualqueseafacil@gmail.com>
2021-09-22 16:10:37 -0300

    crud 1 y 2 realizados

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,14 +4,28 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = require('../utils/toThousand')
+const toDiscount = require('../utils/toDiscount')
+
+
 
 const controller = {
 	index: (req, res) => {
-		// Do the magic
+		return res.render(
+			'index', {
+				visited : products.filter(product => product.category === 'visited'),
+				oferta: products.filter(product => product.category === 'in-sale'),
+				toThousand,
+				toDiscount
+		})
 	},
 	search: (req, res) => {
-		// Do the magic
+		return res.render('results',{
+			products : products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase())),
+			toThousand,
+			toDiscount,
+			keywords: req.query.keywords
+		})
 	},
 };
 
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,41 +4,99 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = require('../utils/toThousand');
+const toDiscount = require('../utils/toDiscount');
 
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+		return res.render('products',{
+			products,
+			toThousand,
+			toDiscount
+		})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+		return res.render('detail',{
+			product : products.find(product => product.id === +req.params.id),
+			toDiscount,
+			toThousand
+		})
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		return res.render('product-create-form')
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+			const {name,price,discount,category,description} = req.body;
+			let lastId = 1;
+
+		products.forEach(product => {
+			if(product.id > lastId){
+				lastId = product.id
+			}
+		})
+
+			let product = {
+				id: lastId + 1 , 
+				name: name.trim(),
+				price: +price ,
+				discount: +discount,
+				category,
+				description: description.trim(),
+				image: 'default-image.png'
+			}
+			products.push(product)
+	
+			fs.writeFileSync(path.join(__dirname,'..', 'data', 'productsDataBase.json'), JSON.stringify(products,null,3), 'utf-8')
+	
+			res.redirect('/products')
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		return res.render('product-edit-form',{
+			product: products.find(product => product.id === +req.params.id)
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		const {name,price,discount,category,description} = req.body;
+
+			let productModified = {
+				id: +req.params.id ,
+				name: name.trim(),
+				price: +price ,
+				discount: +discount,
+				category,
+				description: description.trim(),
+				image: 'default-image.png'
+			}
+			
+			let productsModified = products.map(product => product.id === +req.params.id ? productModified : product)
+	
+			fs.writeFileSync(path.join(__dirname,'..', 'data', 'productsDataBase.json'),JSON.stringify(productsModified,null,3), 'utf-8');
+	
+			res.redirect('/products/detail/' + req.params.id)
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		let productsModified = products.filter(product => product.id !== +req.params.id );
+
+		fs.writeFileSync(path.join(__dirname,'..', 'data', 'productsDataBase.json'),JSON.stringify(productsModified,null,3), 'utf-8');
+
+		res.redirect('/products')
+
 	}
 };
 
